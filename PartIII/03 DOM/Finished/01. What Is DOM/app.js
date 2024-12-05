498e5f53109a6bfadfa1701aa7d8fe4aedbed8f5
boca <ybocajm@gmail.com>
2024-12-04 23:02:23 -0800

    Delete PartIII directory

--- PartIII/03 DOM/Finished/01. What Is DOM/app.js
+++
@@ -1,8 +0,0 @@
-// console.log(document);
-
-console.log(document);
-console.log(document.head);
-console.log(document.title);
-console.log(document.body);
-console.log(document.documentURI);
-console.log(document);
--- PartIII/03 DOM/Finished/07. Style/live coding/app.js
+++
@@ -1,3 +0,0 @@
-const h1 = document.querySelector("h1");
-console.log(h1.style); // GLOBAL CSS STYLES
-h1.style.color = "skyblue";
